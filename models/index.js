5ebaf2588044823fbe9bab6f7a069008829683e3
panimaya albert <42376778+albert0326@users.noreply.github.com>
2020-11-18 20:43:36 +0530

    Update index.js

--- models/index.js
+++ models/index.js
@@ -1,7 +1,7 @@
 var mongoose = require("mongoose");
 
 
-mongoose.connect("mongodb+srv://albert:albert@4444@cluster0.vehvp.mongodb.net/json_api?retryWrites=true&w=majority", {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect("mongodb+srv://albert:***********@cluster0.vehvp.mongodb.net/json_api?retryWrites=true&w=majority", {useNewUrlParser: true, useUnifiedTopology: true});
 mongoose.set("debug", true);
 mongoose.set('useFindAndModify', false);
 	
